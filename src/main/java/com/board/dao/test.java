df484b8f6b15b7e8835d9fcdf2a5cdbab9610154
woo02811 <andy2418@naver.com>
2021-04-05 17:47:32 +0900

    last update

---
+++ src/main/java/com/board/dao/test.java
@@ -0,0 +1,33 @@
+package com.board.dao;
+
+public class test {
+	  	String to;
+	    String priority;
+	    String headTitle;
+	    String headbody;
+		public String getTo() {
+			return to;
+		}
+		public void setTo(String to) {
+			this.to = to;
+		}
+		public String getPriority() {
+			return priority;
+		}
+		public void setPriority(String priority) {
+			this.priority = priority;
+		}
+		public String getHeadTitle() {
+			return headTitle;
+		}
+		public void setHeadTitle(String headTitle) {
+			this.headTitle = headTitle;
+		}
+		public String getHeadbody() {
+			return headbody;
+		}
+		public void setHeadbody(String headbody) {
+			this.headbody = headbody;
+		}
+	    
+}
